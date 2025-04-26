910671708ceb83a790de7a611a2c4b4839529a8e
mrali00 <ali368057@gmail.com>
2025-04-27 02:35:40 +0500

    updated again

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "./", // Ensures relative paths are used for assets
+  build: {
+    outDir: "dist",
+  },
+});
