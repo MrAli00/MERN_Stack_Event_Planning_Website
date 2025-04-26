55bb52f4c7a43010bb62aadfed4e4ed8cb99f225
mrali00 <ali368057@gmail.com>
2025-04-27 00:20:40 +0500

    server update

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,12 @@
 import app from './app.js';
 
+app.get('/',(req,res)=>{
+    res.send({
+        activeStatus:true,
+        error:false,
+    })
+})
+
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
 });
