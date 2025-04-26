12297cf3490b47a64f9081304c8566b0a2c0d0fa
mrali00 <ali368057@gmail.com>
2025-04-26 20:13:05 +0500

    MERN Stack Event Project

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="about container">
+      <h4>Events</h4>
+      <h2>ABOUT</h2>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam
+        exercitationem sed hic error accusantium eaque cumque quo delectus,
+        consequatur commodi quibusdam illum eum quis molestiae. Accusantium
+        excepturi neque possimus tenetur dolorem incidunt, et quas minima
+        provident voluptates tempore exercitationem nesciunt eveniet perferendis
+        molestias non dolorum placeat modi hic dolor veritatis ullam numquam
+        quibusdam! Vero tenetur labore, atque eum incidunt eius doloribus!
+        Expedita voluptatibus modi corrupti enim ab quaerat aut rerum minus.
+        Possimus magnam, quae consequatur repellat nemo a ipsam, reprehenderit
+        dicta quo voluptatem quibusdam in? Consequatur, quae. Repellendus, esse
+        atque at doloremque hic repellat, illo expedita quis culpa deserunt
+        eveniet!
+      </p>
+    </section>
+  );
+};
+
+export default About;
