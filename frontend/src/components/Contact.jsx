46c5fa58a02051d22fa8512008d0d92b3513ce4a
mrali00 <ali368057@gmail.com>
2025-04-27 02:20:33 +0500

    changing in contact

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -12,7 +12,7 @@ const Contact = () => {
     e.preventDefault();
     await axios
       .post(
-        "https://mern-stack-event-planning-project-virid.vercel.app/api/v1/message/send",
+        "http://localhost:4000/api/v1/message/send",
         {
           name,
           email,
